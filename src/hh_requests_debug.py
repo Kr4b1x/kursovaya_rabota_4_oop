29819f87cde32eee3fbcbeb7a7683d67875049c9
Pavel <drago_new@bk.ru>
2024-02-23 15:56:19 +0300

    fix hh_requests_debug.py

--- src/hh_requests_debug.py
+++ src/hh_requests_debug.py
@@ -2,10 +2,17 @@ from src.user_forms import UserForm
 
 
 class HHRequestDebug(UserForm):
+    """
+    Класс для отлавливания ошибок
+    """
     search_query = None
     top_n = None
 
     def user_input_int(self):
+        """
+        Проверка на наличие ошибок ввода
+        :return: integer
+        """
         self.top_n = input("Введите количество вакансий для вывода в top_n: ")
         if self.top_n.isalpha():
             raise ValueError("Количество не может быть строкой")
@@ -16,6 +23,10 @@ class HHRequestDebug(UserForm):
         return int(self.top_n)
 
     def user_input_str(self):
+        """
+        Проверка на наличие ошибок ввода
+        :return: string
+        """
         self.search_query = input("Введите поисковой запрос: ")
         if self.search_query == "":
             raise ValueError("Запрос не может быть пустым")
@@ -23,3 +34,8 @@ class HHRequestDebug(UserForm):
             raise TypeError("Запрос не должен содержать числа")
         else:
             return self.search_query
+
+
+if __name__ == '__main__':
+    r = HHRequestDebug()
+    print(r.user_input_str())
