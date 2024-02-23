bca4848e713153e12ca40c5831f2d4e80e362131
Pavel <drago_new@bk.ru>
2024-02-23 15:55:40 +0300

    fix hh_connection.py

--- src/hh_connection.py
+++ src/hh_connection.py
@@ -1,24 +1,42 @@
 from config import FILE
-from src.API_vacancies import VacancyAPY
+from src.api_vacancies import VacancyAPY
 from src.vacancies import Vacancy
 import json
 import requests
 
 
 class GetHeadHunter(VacancyAPY, Vacancy):
+    """
+    Класс для получения вакансий с HeadHunter
+    с наследованием от VacancyAPY, Vacancy
+    """
     def __init__(self, name, top_n):
+        """
+        Создание экземпляра класса GetHeadHunter
+        с наследованием от VacancyAPY, Vacancy
+        """
         super().__init__(name, top_n)
         self.top_n = top_n
         self.url = "https://api.hh.ru"
 
     def __str__(self):
+        """
+        Магический метод str
+        """
         return f"{self.name}"
 
     def __repr__(self):
+        """
+        Магический метод repr
+        """
         return f"{self.__class__.__name__}({self.name}, {self.top_n})"
 
     @property
     def get_vacancy(self):
+        """
+        Метод чтения вакансий
+        :return: json
+        """
         date = requests.get(f"{self.url}/vacancies",
                             params={'text': self.name,
                                     'area': 113,
@@ -28,6 +46,10 @@ class GetHeadHunter(VacancyAPY, Vacancy):
         return date
 
     def get_json(self):
+        """
+        Метод записи вакансий
+        :return: json
+        """
         with open(FILE, "w", encoding="utf-8") as file:
             file.write(json.dumps(self.get_vacancy, indent=4, ensure_ascii=False))
 
